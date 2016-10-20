dbe7de035c6ec082f164f652c7a5d279f7741902
Matt Langlois <fletchto99@gmail.com>
2016-10-20 12:27:10 -0400

    Use relative paths

--- Copy.sql
+++ Copy.sql
@@ -1,4 +1,4 @@
-\copy Branch from '/home/joel/school/csi3130/project/data/Branch.csv' CSV HEADER;
-\copy Client from '/home/joel/school/csi3130/project/data/Client.csv' CSV HEADER;
-\copy Account from '/home/joel/school/csi3130/project/data/Account.csv' CSV HEADER;
-\copy Owns from '/home/joel/school/csi3130/project/data/Owns.csv' CSV HEADER;
+\copy Branch from 'data/Branch.csv' CSV HEADER;
+\copy Client from 'data/Client.csv' CSV HEADER;
+\copy Account from 'data/Account.csv' CSV HEADER;
+\copy Owns from 'data/Owns.csv' CSV HEADER;
