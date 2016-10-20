7cb4037feb4eff778854fd928af8af7f9c75fe4e
Matt Langlois <fletchto99@gmail.com>
2016-10-20 12:11:35 -0400

    Fix creation script

--- createtable.sql
+++ createtable.sql
@@ -1,41 +1,27 @@
-﻿CREATE SCHEMA Banking AUTHORIZATION joel;
-SET SCHEMA 'banking';
-
---CREATE DOMAIN marital_status_domain ();
---CREATE DOMAIN postal_code_ca_domain ();
-
-CREATE TABLE Client (
-client_number int,
-lastname varchar(30),
-firstname varchar(30),
-marital_status varchar(10),
-postal_code varchar(8),
-phone varchar(10),
-city varchar(20)
+﻿CREATE TABLE Client (
+	client_number SERIAL PRIMARY KEY,
+	lastname VARCHAR(30),
+	firstname VARCHAR(30),
+	marital_status VARCHAR(10),
+	postal_code VARCHAR(8),
+	phone VARCHAR(10),
+	city VARCHAR(20)
 );
 
 CREATE TABLE Account (
-account_number int,
-dollar_balance numeric(15,2),
-branch_number int
-);
-
-CREATE TABLE Owns (
-account_number int,
-client_number int
+	account_number SERIAL PRIMARY KEY,
+	dollar_balance NUMERIC(15,2),
+	branch_number INT
 );
 
 CREATE TABLE Branch (
-branch_number int,
-branch_name varchar(30),
-city varchar(30)
+	branch_number SERIAL PRIMARY KEY,
+	branch_name VARCHAR(30),
+	city VARCHAR(30)
 );
 
-ALTER TABLE Client ADD PRIMARY KEY (client_number);
-ALTER TABLE Account ADD PRIMARY KEY (account_number);
-ALTER TABLE Owns ADD PRIMARY KEY (client_number, account_number);
-ALTER TABLE Branch ADD PRIMARY KEY (branch_number);
-
-ALTER TABLE Owns ADD CONSTRAINT accountfk FOREIGN KEY (account_number) REFERENCES Account (account_number) MATCH FULL;
-ALTER TABLE Owns ADD CONSTRAINT clientfk FOREIGN KEY (client_number) REFERENCES Client (client_number) MATCH FULL;
-ALTER TABLE Account ADD CONSTRAINT branchfk FOREIGN KEY (branch_number) REFERENCES Branch (branch_number) MATCH FULL;
+CREATE TABLE Owns (
+	account_number INT REFERENCES Account(account_number),
+	client_number INT REFERENCES Client(client_number),
+	PRIMARY KEY (client_number, account_number)
+);
