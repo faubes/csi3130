2aa8f478ad266223e5dab10efc08497767315ebb
Matt Langlois <fletchto99@gmail.com>
2016-11-10 12:42:22 -0500

    Minor fixes for queries and rollback by default

--- partb.sql
+++ partb.sql
@@ -12,59 +12,57 @@ BEGIN;
 INSERT INTO Account VALUES (447712, 300.00, 106);
 INSERT INTO Client VALUES (999433, 'Rasmussen', 'Igor', 'Single', 'V6A 0X8', 2505556969, 'Victoria');
 INSERT INTO Owns VALUES (447712, 999433);
-COMMIT;
+ROLLBACK;
 
 -- T2
 BEGIN;
 UPDATE Account SET dollar_balance=(dollar_balance - 275.00) WHERE account_number=447712; 
-COMMIT:
+ROLLBACK;
 
 -- T3
 BEGIN; 
 UPDATE Account SET dollar_balance = dollar_balance + 2800 WHERE account_number = 4477212;
 UPDATE Account SET dollar_balance = dollar_balance - 2800 WHERE account_number = 424244;
-COMMIT;
+ROLLBACK;
 
 -- T4
 BEGIN;
 UPDATE Account SET dollar_balance = dollar_balance - 1000000 WHERE account_number = 445128;
 UPDATE Account SET dollar_balance = dollar_balance + 1000000 WHERE account_number = 4477212;
-COMMIT;
+ROLLBACK;
 
 -- T5
 BEGIN; 
 UPDATE Client SET marital_status='Married', postal_code='M3B 3R5‎', phone=4162345432 WHERE client_number = 999433;
 UPDATE Account SET branch_number = 100 WHERE account_number = (SELECT account_number FROM Owns WHERE client_number = 999433);
-COMMIT;
+ROLLBACK;
 
 -- T6
 BEGIN;
 SELECT SUM(dollar_balance) FROM Account WHERE branch_number=
 (SELECT branch_number FROM Branch WHERE city='Toronto' AND branch_name='Downtown');
-COMMIT;
+ROLLBACK;
 
 -- T7
 BEGIN;
 SELECT AVG(dollar_balance) AS Average_Balance FROM Account WHERE branch_number = 
 (SELECT branch_number FROM Branch WHERE city='Calgary' AND branch_name='Downtown');
-COMMIT;
+ROLLBACK;
 
 -- T8
 BEGIN;
-UPDATE Account SET dollar_balance = dollar_balance*1.07 WHERE dollar_balance > 400,000.00
-COMMIT;
+UPDATE Account SET dollar_balance = dollar_balance*1.07 WHERE dollar_balance > 400000.00;
+ROLLBACK;
 
 -- T9
 BEGIN;
 INSERT INTO account(account_number, dollar_balance, branch_number) VALUES 
 (425450, 600000.00, (SELECT branch_number FROM Branch WHERE city='Toronto' AND branch_name='Downtown'));
 UPDATE account SET dollar_balance = dollar_balance - 600000 WHERE account_number = 445128;
-COMMIT;
+ROLLBACK;
 
 -- T10
 --Igor withdraws $550,000.00, in cash, from the newly opened joint account.
 BEGIN;
 UPDATE account SET dollar_balance = dollar_balance - 550000 WHERE account_number = 425450;
-COMMIT;
-
-
+ROLLBACK;
